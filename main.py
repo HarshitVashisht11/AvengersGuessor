0c8999110274e1aadab3feacfd81abec6871cb4a
HarshitVashisht11 <harshitvashisht07@gmail.com>
2024-03-14 01:18:01 +0530

    Update upload page and route

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def allowed_file(filename):
 
 @app.route('/')
 def upload_form():
-    return render_template('upload.html')
+    return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         flash('No file part')
